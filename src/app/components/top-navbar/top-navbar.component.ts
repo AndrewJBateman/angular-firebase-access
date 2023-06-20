565b961a76e26a87cc600c915777616cc4d5b656
AndrewJBateman <gomezbateman@yahoo.com>
2023-06-20 20:50:59 +0200

    refactor: remove fxflex & unused code

--- src/app/components/top-navbar/top-navbar.component.ts
+++ src/app/components/top-navbar/top-navbar.component.ts
@@ -1,15 +1,11 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
-import { AuthService } from "../../services/auth.service";
-
 @Component({
   selector: "app-top-navbar",
   templateUrl: "./top-navbar.component.html",
   styleUrls: ["./top-navbar.component.scss"],
 })
 export class TopNavbarComponent {
-  loggedIn = false;
-
-  constructor(public auth: AuthService, public router: Router) {}
+  constructor(public router: Router) {}
 }
