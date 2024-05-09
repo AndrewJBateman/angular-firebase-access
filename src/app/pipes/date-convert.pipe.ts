bbd73b0a709826b4607898967b30ebf379608699
AndrewJBateman <gomezbateman@yahoo.com>
2024-05-09 18:16:00 +0200

    fix: correct time ago to convert firestore time stamp, footer spacings

--- src/app/pipes/date-convert.pipe.ts
+++ src/app/pipes/date-convert.pipe.ts
@@ -1,15 +1,19 @@
 import { Pipe, PipeTransform } from "@angular/core";
 import dayjs from "dayjs";
+import { Timestamp } from "firebase/firestore";
 import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(relativeTime);
 
 @Pipe({
-    name: "dateConvert",
-    standalone: true,
+  name: "dateConvert",
+  standalone: true,
 })
 export class DateConvertPipe implements PipeTransform {
-  transform(value: any): string {
-    return dayjs(value).fromNow();
+  transform(timestamp: Timestamp): string {
+    const date = timestamp?.toDate();
+    let result = "";
+    result = dayjs(date).fromNow();
+    return result;
   }
 }
